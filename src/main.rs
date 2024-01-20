6e8780113be5fccb061b3b06ea38ea045e3fcb8d
Allen Dai <zdai0114@gmail.com>
2024-01-20 11:31:43 -0500

    updated depen

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-
-use std::sync::{Weak, Arc};
-
 use storm::prelude::*;
 
 fn main() {
-    let a = Tensor::_arange(-10.0, 10., 1.).pow(9.9, false);
+    let a = Tensor::from([-0.314]).pow(0.314, false);
     println!("{:?}", a.to_vec());
 }
